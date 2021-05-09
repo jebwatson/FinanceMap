9346f3a48b17ed131d455c1bec777cff7f4f7a69
Jeb Watson <jeb.watson@gmail.com>
2021-05-09 13:02:07 -0500

    Fix the unit tests

--- FinanceMapTests/FinanceMapCoreTests/ProjectAccountValueTests.cs
+++ FinanceMapTests/FinanceMapCoreTests/ProjectAccountValueTests.cs
@@ -7,143 +7,275 @@ namespace FinanceMapTests.FinanceMapCoreTests
     public class ProjectAccountValueTests
     {
         private static readonly DateTime Today = DateTime.Today;
-        public record ProjectAccountValueTestCase
-        {
-            public Account Account { get; init; }
-            public DateTime NextPayday { get; init; }
-            public DateTime ProjectionDate { get; init; }
-            public AccountEntry AccountEntry { get; init; }
-            public Account ProjectedAccount { get; init; }
-        }
 
         private static object[] testCases =
         {
             // Project to next payday
             new ProjectAccountValueTestCase
             {
-                Account = new Account() { Value = 500 },
-                NextPayday = Today + TimeSpan.FromDays(14),
-                ProjectionDate = Today + TimeSpan.FromDays(14),
-                AccountEntry = new AccountEntry() { Value = 500, Frequency = TimeSpan.FromDays(14) },
-                ProjectedAccount = new Account() { Value = 1000 }
+                Projection = new Projection
+                {
+                    Account = new Account
+                    {
+                        Value = 500,
+                        FixedRecurringIncome = new AccountEntry
+                        {
+                            Value = 500,
+                            Frequency = TimeSpan.FromDays(14)
+                        }
+                    },
+                    ProjectedAccountValue = 1000,
+                    NextPayday = Today + TimeSpan.FromDays(14),
+                    Date = Today + TimeSpan.FromDays(14)
+                }
             },
             // Project to next payday
             new ProjectAccountValueTestCase
             {
-                Account = new Account() { Value = 500 },
-                NextPayday = Today + TimeSpan.FromDays(3),
-                ProjectionDate = Today + TimeSpan.FromDays(3),
-                AccountEntry = new AccountEntry() { Value = 500, Frequency = TimeSpan.FromDays(14) },
-                ProjectedAccount = new Account() { Value = 1000 }
+                Projection = new Projection
+                {
+                    Account = new Account
+                    {
+                        Value = 500,
+                        FixedRecurringIncome = new AccountEntry
+                        {
+                            Value = 500,
+                            Frequency = TimeSpan.FromDays(14)
+                        }
+                    },
+                    ProjectedAccountValue = 1000,
+                    NextPayday = Today + TimeSpan.FromDays(3),
+                    Date = Today + TimeSpan.FromDays(3)
+                }
             },
             // Project before next payday
             new ProjectAccountValueTestCase
             {
-                Account = new Account() { Value = 500 },
-                NextPayday = Today + TimeSpan.FromDays(14),
-                ProjectionDate = Today + TimeSpan.FromDays(13),
-                AccountEntry = new AccountEntry() { Value = 500, Frequency = TimeSpan.FromDays(14) },
-                ProjectedAccount = new Account() { Value = 500 }
+                Projection = new Projection
+                {
+                    Account = new Account
+                    {
+                        Value = 500,
+                        FixedRecurringIncome = new AccountEntry
+                        {
+                            Value = 500,
+                            Frequency = TimeSpan.FromDays(14)
+                        }
+                    },
+                    ProjectedAccountValue = 500,
+                    NextPayday = Today + TimeSpan.FromDays(14),
+                    Date = Today + TimeSpan.FromDays(13)
+                }
             },
             // Project before next payday
             new ProjectAccountValueTestCase
             {
-                Account = new Account() { Value = 500 },
-                NextPayday = Today + TimeSpan.FromDays(3),
-                ProjectionDate = Today + TimeSpan.FromDays(2),
-                AccountEntry = new AccountEntry() { Value = 500, Frequency = TimeSpan.FromDays(14) },
-                ProjectedAccount = new Account() { Value = 500 }
+                Projection = new Projection
+                {
+                    Account = new Account
+                    {
+                        Value = 500,
+                        FixedRecurringIncome = new AccountEntry
+                        {
+                            Value = 500,
+                            Frequency = TimeSpan.FromDays(14)
+                        }
+                    },
+                    ProjectedAccountValue = 500,
+                    NextPayday = Today + TimeSpan.FromDays(3),
+                    Date = Today + TimeSpan.FromDays(2)
+                }
             },
             // Project to next payday + 1 pay period
             new ProjectAccountValueTestCase
             {
-                Account = new Account() { Value = 500 },
-                NextPayday = Today + TimeSpan.FromDays(14),
-                ProjectionDate = Today + TimeSpan.FromDays(28),
-                AccountEntry = new AccountEntry() { Value = 500, Frequency = TimeSpan.FromDays(14) },
-                ProjectedAccount = new Account() { Value = 1500 }
+                Projection = new Projection
+                {
+                    Account = new Account
+                    {
+                        Value = 500,
+                        FixedRecurringIncome = new AccountEntry
+                        {
+                            Value = 500,
+                            Frequency = TimeSpan.FromDays(14)
+                        }
+                    },
+                    ProjectedAccountValue = 1500,
+                    NextPayday = Today + TimeSpan.FromDays(14),
+                    Date = Today + TimeSpan.FromDays(28)
+                }
             },
             // Project to next payday + 1 pay period
             new ProjectAccountValueTestCase
             {
-                Account = new Account() { Value = 500 },
-                NextPayday = Today + TimeSpan.FromDays(3),
-                ProjectionDate = Today + TimeSpan.FromDays(17),
-                AccountEntry = new AccountEntry() { Value = 500, Frequency = TimeSpan.FromDays(14) },
-                ProjectedAccount = new Account() { Value = 1500 }
+                Projection = new Projection
+                {
+                    Account = new Account
+                    {
+                        Value = 500,
+                        FixedRecurringIncome = new AccountEntry
+                        {
+                            Value = 500,
+                            Frequency = TimeSpan.FromDays(14)
+                        }
+                    },
+                    ProjectedAccountValue = 1500,
+                    NextPayday = Today + TimeSpan.FromDays(3),
+                    Date = Today + TimeSpan.FromDays(17)
+                }
             },
             // Project to next payday + a few days
             new ProjectAccountValueTestCase
             {
-                Account = new Account() { Value = 500 },
-                NextPayday = Today + TimeSpan.FromDays(14),
-                ProjectionDate = Today + TimeSpan.FromDays(17),
-                AccountEntry = new AccountEntry() { Value = 500, Frequency = TimeSpan.FromDays(14) },
-                ProjectedAccount = new Account() { Value = 1000 }
+                Projection = new Projection
+                {
+                    Account = new Account
+                    {
+                        Value = 500,
+                        FixedRecurringIncome = new AccountEntry
+                        {
+                            Value = 500,
+                            Frequency = TimeSpan.FromDays(14)
+                        }
+                    },
+                    ProjectedAccountValue = 1000,
+                    NextPayday = Today + TimeSpan.FromDays(14),
+                    Date = Today + TimeSpan.FromDays(17)
+                }
             },
             // Project to next payday + a few days
             new ProjectAccountValueTestCase
             {
-                Account = new Account() { Value = 500 },
-                NextPayday = Today + TimeSpan.FromDays(3),
-                ProjectionDate = Today + TimeSpan.FromDays(5),
-                AccountEntry = new AccountEntry() { Value = 500, Frequency = TimeSpan.FromDays(14) },
-                ProjectedAccount = new Account() { Value = 1000 }
+                Projection = new Projection
+                {
+                    Account = new Account
+                    {
+                        Value = 500,
+                        FixedRecurringIncome = new AccountEntry
+                        {
+                            Value = 500,
+                            Frequency = TimeSpan.FromDays(14)
+                        }
+                    },
+                    ProjectedAccountValue = 1000,
+                    NextPayday = Today + TimeSpan.FromDays(3),
+                    Date = Today + TimeSpan.FromDays(5)
+                }
             },
             // Project to today
             new ProjectAccountValueTestCase
             {
-                Account = new Account() { Value = 500 },
-                NextPayday = Today + TimeSpan.FromDays(14),
-                ProjectionDate = Today,
-                AccountEntry = new AccountEntry() { Value = 500, Frequency = TimeSpan.FromDays(14) },
-                ProjectedAccount = new Account() { Value = 500 }
+                Projection = new Projection
+                {
+                    Account = new Account
+                    {
+                        Value = 500,
+                        FixedRecurringIncome = new AccountEntry
+                        {
+                            Value = 500,
+                            Frequency = TimeSpan.FromDays(14)
+                        }
+                    },
+                    ProjectedAccountValue = 500,
+                    NextPayday = Today + TimeSpan.FromDays(14),
+                    Date = Today
+                }
             },
             // Project to today
             new ProjectAccountValueTestCase
             {
-                Account = new Account() { Value = 500 },
-                NextPayday = Today + TimeSpan.FromDays(3),
-                ProjectionDate = Today,
-                AccountEntry = new AccountEntry() { Value = 500, Frequency = TimeSpan.FromDays(14) },
-                ProjectedAccount = new Account() { Value = 500 }
+                Projection = new Projection
+                {
+                    Account = new Account
+                    {
+                        Value = 500,
+                        FixedRecurringIncome = new AccountEntry
+                        {
+                            Value = 500,
+                            Frequency = TimeSpan.FromDays(14)
+                        }
+                    },
+                    ProjectedAccountValue = 500,
+                    NextPayday = Today + TimeSpan.FromDays(3),
+                    Date = Today
+                }
             },
             // Project to past date
             new ProjectAccountValueTestCase
             {
-                Account = new Account() { Value = 500 },
-                NextPayday = Today + TimeSpan.FromDays(14),
-                ProjectionDate = Today - TimeSpan.FromDays(2),
-                AccountEntry = new AccountEntry() { Value = 500, Frequency = TimeSpan.FromDays(14) },
-                ProjectedAccount = new Account() { Value = 500 }
+                Projection = new Projection
+                {
+                    Account = new Account
+                    {
+                        Value = 500,
+                        FixedRecurringIncome = new AccountEntry
+                        {
+                            Value = 500,
+                            Frequency = TimeSpan.FromDays(14)
+                        }
+                    },
+                    ProjectedAccountValue = 500,
+                    NextPayday = Today + TimeSpan.FromDays(14),
+                    Date = Today - TimeSpan.FromDays(2)
+                }
             },
             // Project to past date
             new ProjectAccountValueTestCase
             {
-                Account = new Account() { Value = 500 },
-                NextPayday = Today + TimeSpan.FromDays(3),
-                ProjectionDate = Today - TimeSpan.FromDays(2),
-                AccountEntry = new AccountEntry() { Value = 500, Frequency = TimeSpan.FromDays(14) },
-                ProjectedAccount = new Account() { Value = 500 }
+                Projection = new Projection
+                {
+                    Account = new Account
+                    {
+                        Value = 500,
+                        FixedRecurringIncome = new AccountEntry
+                        {
+                            Value = 500,
+                            Frequency = TimeSpan.FromDays(14)
+                        }
+                    },
+                    ProjectedAccountValue = 500,
+                    NextPayday = Today + TimeSpan.FromDays(3),
+                    Date = Today - TimeSpan.FromDays(2)
+                }
             },
             // Payday in the past
             new ProjectAccountValueTestCase
             {
-                Account = new Account() { Value = 500 },
-                NextPayday = Today - TimeSpan.FromDays(14),
-                ProjectionDate = Today + TimeSpan.FromDays(2),
-                AccountEntry = new AccountEntry() { Value = 500, Frequency = TimeSpan.FromDays(14) },
-                ProjectedAccount = new Account() { Value = 500 }
+                Projection = new Projection
+                {
+                    Account = new Account
+                    {
+                        Value = 500,
+                        FixedRecurringIncome = new AccountEntry
+                        {
+                            Value = 500,
+                            Frequency = TimeSpan.FromDays(14)
+                        }
+                    },
+                    ProjectedAccountValue = 500,
+                    NextPayday = Today - TimeSpan.FromDays(14),
+                    Date = Today + TimeSpan.FromDays(2)
+                }
             },
             // Payday in the past
             new ProjectAccountValueTestCase
             {
-                Account = new Account() { Value = 500 },
-                NextPayday = Today - TimeSpan.FromDays(3),
-                ProjectionDate = Today + TimeSpan.FromDays(2),
-                AccountEntry = new AccountEntry() { Value = 500, Frequency = TimeSpan.FromDays(14) },
-                ProjectedAccount = new Account() { Value = 500 }
-            },
+                Projection = new Projection
+                {
+                    Account = new Account
+                    {
+                        Value = 500,
+                        FixedRecurringIncome = new AccountEntry
+                        {
+                            Value = 500,
+                            Frequency = TimeSpan.FromDays(14)
+                        }
+                    },
+                    ProjectedAccountValue = 500,
+                    NextPayday = Today - TimeSpan.FromDays(3),
+                    Date = Today + TimeSpan.FromDays(2)
+                }
+            }
         };
 
         [TestCaseSource(nameof(testCases))]
@@ -151,12 +283,20 @@ namespace FinanceMapTests.FinanceMapCoreTests
         {
             var uut = new AccountProjectionService();
             var result = AccountProjectionService.ForwardProjectFixedIncomeToAccountValue(
-                tc.Account,
-                tc.NextPayday,
-                tc.ProjectionDate,
-                tc.AccountEntry);
-            
-            Assert.AreEqual(tc.ProjectedAccount, result);
+                tc.Projection.Account,
+                tc.Projection.NextPayday,
+                tc.Projection.Date,
+                tc.Projection.Account.FixedRecurringIncome);
+
+            Assert.AreEqual(
+                tc.Projection.ProjectedAccountValue,
+                result.ProjectedAccountValue,
+                0.0000001);
+        }
+
+        public record ProjectAccountValueTestCase
+        {
+            public Projection Projection { get; init; }
         }
     }
 }
